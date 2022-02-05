0cb3daadfc8e283b4108c29e43d96b0053699da6
wilas0o0o <03mcu30@gmail.com>
2022-02-05 09:05:45 +0000

    [Start]posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,34 +3,51 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def update
-
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
     def posts_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:user_id, :situation_id, :text)
     end
 
     def ensure_correct_user
       @post = Post.find(params[:id])
-      unless @post.user == current_user
+      unless @post.user_id == current_user.id
         redirect_to book_path(@book)
       end
     end
