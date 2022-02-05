c0466eb9e152f444be2d0a6e6b93a98d57e8ebca
wilas0o0o <03mcu30@gmail.com>
2022-02-05 10:38:10 +0000

    [Add]posts_edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :text, presence: true, length: { maximum: 140 }
+  validates :situation, presence: true
 
   enum situation: { working: 0, gaming: 1 }
 end
