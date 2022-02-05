076230679797cbb76ac341c6d3c5ba29f01255b4
wilas0o0o <03mcu30@gmail.com>
2022-02-05 04:02:36 +0000

    [Add]users_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,10 +10,19 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
-  
+
   private
-  
+
+    def user_params
+      params.require(:user).permit(:name, :email, :avatar)
+    end
+
     def ensure_correct_user
       @user = User.find(params[:id])
       unless @user == current_user
