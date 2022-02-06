8573dd4387bc207b3582af5724cb9776bbdfee1b
wilas0o0o <03mcu30@gmail.com>
2022-02-06 03:58:46 +0000

    [Start]bookmarks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   mount_uploaders :images, ImageUploader
 
@@ -13,5 +14,9 @@ class Post < ApplicationRecord
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+  end
+
+  def bookmarked_by(user)
+    bookmarks.where(user_id: user.id).exists?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_024546) do
+ActiveRecord::Schema.define(version: 2022_02_06_035402) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_bookmarks_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_bookmarks_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "post_id", null: false
