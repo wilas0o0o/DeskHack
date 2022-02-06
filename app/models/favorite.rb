ec3c2d0eae4f23d0aac71ce4ba305a82a54bd038
wilas0o0o <03mcu30@gmail.com>
2022-02-06 03:17:39 +0000

    [Add]favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,8 @@ class Favorite < ApplicationRecord
   belongs_to :post
 
   validates_uniqueness_of :post_id, scope: :user_id
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
