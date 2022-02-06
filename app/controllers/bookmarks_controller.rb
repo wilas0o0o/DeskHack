68feeb8185c1684fcbd1822b6e7736881696f376
wilas0o0o <03mcu30@gmail.com>
2022-02-06 04:41:17 +0000

    [Add]ajax_bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,13 +5,11 @@ class BookmarksController < ApplicationController
     @post = Post.find(params[:post_id])
     bookmark = @post.bookmarks.new(user_id: current_user.id)
     bookmark.save
-    redirect_to request.referer
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     bookmark = @post.bookmarks.find_by(user_id: current_user.id)
     bookmark.destroy
-    redirect_to request.referer
   end
 end
