517a26ba3d8015fa814e863a057463103102ec82
wilas0o0o <03mcu30@gmail.com>
2022-02-06 13:12:56 +0000

    [Add]comment_ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,11 +6,9 @@ class PostCommentsController < ApplicationController
     @post_comment = @post.post_comments.new(post_comment_params)
     @post_comment.user_id = current_user.id
     if @post_comment.save
-      redirect_to request.referer
     else
-      binding.pry
       @post = Post.find(params[:post_id])
-      render 'posts/show'
+      render :error
     end
   end
 
@@ -18,7 +16,6 @@ class PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     post_comment = @post.post_comments.find(params[:id])
     post_comment.destroy
-    redirect_to request.referer
   end
 
   private
