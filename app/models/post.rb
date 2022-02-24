0f980b9a03418479d08819e42d06916b9fb7b5dc
wilas0o0o <03mcu30@gmail.com>
2022-02-24 08:29:44 +0000

    [Fix]notification_error

--- app/models/post.rb
+++ app/models/post.rb
@@ -84,20 +84,18 @@ class Post < ApplicationRecord
 
   # コメントの通知を作成
   def create_notification_post_comment!(current_user, post_comment_id)
-    # 自分以外にコメントしている人を全て取得し全員に通知を送る
+    # コメントしている人がいれば全員に通知を送る
     temp_ids = PostComment.select(:user_id).
-      where(post_id: id).
-      where.not(user_id: current_user.id).distinct
+      where(post_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
-      save_notification_post_comment!(current_user, post_comment_id, temp_id("user_id"))
+      save_notification_post_comment!(current_user, post_comment_id, temp_id["user_id"])
     end
-    # まだ誰もコメントしていない場合は投稿者のみに通知を送る
-    save_notification_post_comment!(current_user, post_comment_id, user_id) if temp_ids.blank?
+    # 投稿者のみに通知を送る
+    save_notification_post_comment!(current_user, post_comment_id, user_id)
   end
 
   # 作成したコメントの通知を保存
   def save_notification_post_comment!(current_user, post_comment_id, visited_id)
-    # 同じ投稿に複数コメントすることがあるので、1つの投稿に複数回通知する
     notification = current_user.active_notifications.new(
       post_id: id,
       post_comment_id: post_comment_id,
