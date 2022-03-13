f2ca27d55c1d348ea5280734c8e259b979b48a4a
wilas0o0o <03mcu30@gmail.com>
2022-03-13 01:53:03 +0000

    [Finish]from_user/show

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -23,13 +23,15 @@ class RelationshipsController < ApplicationController
   end
 
   def ajax_create
-    user = User.find_by(username: params[:user_id])
-    current_user.follow(user)
-    user.create_notification_follow!(current_user)
+    @user = User.find_by(username: params[:user_id])
+    current_user.follow(@user)
+    @user.create_notification_follow!(current_user)
+    render :create
   end
 
   def ajax_destroy
-    user = User.find_by(username: params[:user_id])
-    current_user.unfollow(user)
+    @user = User.find_by(username: params[:user_id])
+    current_user.unfollow(@user)
+    render :destroy
   end
 end
