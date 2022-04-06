90aae1c3f952663a344b28e483fa40c24e8d7f77
wilas0o0o <03mcu30@gmail.com>
2022-04-06 07:29:27 +0000

    [Add]ja_error

--- app/models/user.rb
+++ app/models/user.rb
@@ -104,7 +104,6 @@ class User < ApplicationRecord
 
   # SNS認証メソッド
   def self.from_omniauth(auth)
-    binding.pry
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
       name: auth.info.name,
